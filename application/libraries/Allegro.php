8ddf318485ed115f0f6eedd0e362583573ab8fe0
Jan Bogdanski <jan.b@aol.pl>
2012-02-03 18:52:42 +0100

    initial commit!

---
+++ application/libraries/Allegro.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Allegro WebApi client
+ * 
+ * @author          Jan Bogdanski <janek.bogdanski@gmail.com>
+ * @package         Proaukcje Kreator Aukcji
+ * @see             https://github.com/janbogdanski/codeigniter-allegro-webapi
+ * Creation date    03.02.12 18:49
+ */
+class Allegro extends CI_Controller {
+    
+}
+
+

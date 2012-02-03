39cc73fef4d7400274bbd3f34041bc83974e4783
Jan Bogdanski <jan.b@aol.pl>
2012-02-03 20:25:33 +0100

    allegro: dodanie pliku konfiguracyjnego z domyslnymi danymi dostepu

---
+++ application/config/allegro.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author          Jan Bogdanski <janek.bogdanski@gmail.com>
+ * @package         Proaukcje Kreator Aukcji
+ *
+ * Creation date    03.02.12 20:20
+ */
+
+/**
+ * Dane dostepu do allegro, ustawienie domyslne, mozliwe przekazanie w konstruktorze innych
+ */
+$config['login'] = '';
+$config['password'] = '';
+$config['api_key'] = '';
+$config['wsdl'] = '';
+$config['country'] = '';
